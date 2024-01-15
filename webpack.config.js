535b2d7c0664206f33e7858f28c921dede2617c1
Cosarca Maxim <cosarcamaxim@gmail.com>
2024-01-15 18:47:59 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');  
+
+const isProd = process.env.NODE_ENV === "production"
+const isDev = !isProd
+
+const path = require('path');
+
+
+
+module.exports = {
+  // parser: "@babel/eslint-parser",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          // "style-loader",
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader:"babel-loader" ,
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ],
+  },
+  context: path.resolve(__dirname, 'src'),
+  
+  mode: 'development',
+  entry: '/index.js', 
+  output: {
+    filename: 'bundle[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve : {
+    extensions : ['.js'],
+    alias : {
+      '@' : path.resolve(__dirname , 'src'),
+      '@core' :path.resolve(__dirname , 'src/core')
+    }
+  },
+  devtool : isDev ? "source-map" : false, 
+  devServer : {
+    port : 3000,
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new HtmlWebpackPlugin(
+      {
+        template : "index.html",
+        minify : {
+          removeComments : isProd,
+          collapseWhitespace : isProd,
+        }
+      }
+
+    ),
+    new CopyPlugin({
+      patterns: [
+          { 
+            from: path.resolve(__dirname, "src/favicon.ico"),
+            to: path.resolve(__dirname, 'dist')
+          }
+        ],
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename : "bundle.[hash].css"
+    })
+  ],
+};
+
+
+
+
+
+
+
