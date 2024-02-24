116970cde444deaf7cd2d5d63858d8ffe4b5181a
literallyNotFate <dxfinitelynotfate@gmail.com>
2024-02-24 10:06:28 +0200

    add table selector

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,22 @@
 import {ExcelComponent} from '../../core/ExcelComponent';
 import {createTable} from './table.template';
 import resizeHandler from './table.resize';
+import {TableSelection} from './TableSelection';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
   constructor($root) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown'],
+      listeners: ['mousedown', 'mouseup', 'mousemove'],
     });
+  }
+  onMouseup() {}
+  onMousemove() {}
+
+  init() {
+    super.init();
+    this.selection = new TableSelection();
   }
 
   onMousedown(event) {
